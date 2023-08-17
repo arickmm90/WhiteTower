d7d8751ca382ab4bf225da3fee36270d0f702291
Arick <arickmm90@gmail.com>
2023-08-16 22:21:38 -0400

    my first commit

--- src/TestTodoMVC/MyTestingClass.java
+++ src/TestTodoMVC/MyTestingClass.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import org.testng.annotations.Listeners;
 import org.testng.asserts.Assertion;
 import org.openqa.selenium.support.ui.Select;
 
@@ -35,8 +36,8 @@ public class MyTestingClass {
         driver.findElement(By.linkText("AngularJS")).click();
         expectedResult="AngularJS • TodoMVC";
         actualResult=driver.getTitle();
-        Assert.assertEquals(expectedResult, actualResult,"Titulo diferente");
-        System.out.println("Encontre AngularJS " + driver.getTitle());
+        Assert.assertEquals(expectedResult, actualResult,"This is not the correct page");
+        System.out.println("I found " + driver.getTitle());
 
         //Para llenar la lista
         WebElement todoList = driver.findElement(By.xpath("/html/body/ng-view/section/header/form/input"));
@@ -52,70 +53,28 @@ public class MyTestingClass {
                 .build();
         seriesActions.perform();
         Thread.sleep(3000);
-        System.out.println("Se han agregado los 3 ToDos exitosamente");
+        System.out.println("ToDos have been succefully added to the list");
     }
-
-    /*@BeforeMethod
-    public void findLink(){
-        driver.findElement(By.linkText("AngularJS")).click();
-        expectedResult="AngularJS • TodoMVC";
-        actualResult=driver.getTitle();
-        Assert.assertEquals(expectedResult, actualResult,"Titulo diferente");
-        System.out.println("Encontre AngularJS " + driver.getTitle());
-    }*/
 
     @AfterMethod
     public void Quit(){
-        System.out.println("Vamos a cerrar el navegador");
+        System.out.println("Let's close the browser");
         driver.quit();
     }
 
     @Test
     public void testcase1()  {
-        /*WebElement todoList = driver.findElement(By.xpath("/html/body/ng-view/section/header/form/input"));
+        System.out.println("Starting test case 1");
 
-        Actions builder = new Actions(driver);
-        Action seriesActions = builder.moveToElement(todoList)
-                .click()
-                .sendKeys("ToDo 1")
-                .sendKeys(Keys.ENTER)
-                .sendKeys("ToDo 2")
-                .sendKeys(Keys.ENTER)
-                .sendKeys("ToDo 3")
-                .sendKeys(Keys.ENTER)
-                .build();
-        seriesActions.perform();
-        Thread.sleep(3000);
-        System.out.println("Se han agregado los 3 ToDos exitosamente");*/
-
+        //Verify that the element ToDo 3 is in the 3rd position of the list
         String thirdposition= driver.findElement(By.xpath("/html/body/ng-view/section/section/ul/li[3]/div/label")).getText();
         Assert.assertEquals(thirdposition, "ToDo 3", "ToDo 3 is not in third position");
-        System.out.println("En la tercera posicion esta: "+ thirdposition);
-
+        System.out.println("The element in the third position is > "+ thirdposition);
     }
 
     @Test
     public void testcase2() throws InterruptedException {
-        System.out.println("Aqui va el test 2");
-
-        /*Actions act = new Actions(driver);
-        WebElement ele = driver.findElement(By.xpath("/html/body/ng-view/section/section/ul/li[2]/div/label"));
-        act.doubleClick(ele).perform();
-        ele.sendKeys(Keys.BACK_SPACE);
-        ele.sendKeys(Keys.BACK_SPACE);
-        Thread.sleep(1000);
-        ele.sendKeys(Keys.BACK_SPACE);
-        ele.sendKeys(Keys.BACK_SPACE);
-        Thread.sleep(1000);
-        ele.sendKeys(Keys.BACK_SPACE);
-        ele.sendKeys(Keys.BACK_SPACE);
-        Thread.sleep(2000);
-        ele.sendKeys("ToDO 4");
-        ele.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
-        System.out.println(ele.getText()+"Yo");*/
-
-
+        System.out.println("Starting test case 2");
 
         WebElement secondPosition = driver.findElement(By.xpath("/html/body/ng-view/section/section/ul/li[2]/div/label"));
 
@@ -134,25 +93,24 @@ public class MyTestingClass {
         seriesAction.perform();
         Thread.sleep(3000);
 
-        //Para verificar si el ToDo4 se encuentra ahora en la 2da posicion de la lista
-        Assert.assertEquals("ToDo 4", secondPosition.getText(), "ToDo 4 no esta en la 2da posicion");
-        System.out.println("En la 2da posicion esta> "+ secondPosition.getText());
+        //Verify that the element “ToDo 4” is now in the 2 nd position of the list
+        Assert.assertEquals("ToDo 4", secondPosition.getText(), "ToDo 4 is not in 2nd position");
+        System.out.println("The element in 2nd position is > "+ secondPosition.getText());
 
-        //Para verificar si ToDo2 esta en alguna posicion de la lista
+        //Verify that the element “ToDo 2” does not appear in the list
         for (int i=1; i<4; i++){
             String element = driver.findElement(By.xpath("/html/body/ng-view/section/section/ul/li["+ i + "]/div/label")).getText();
             if (element == "ToDo 2"){
-                System.out.println("El ToDo 2 se encuentra en la posicion " + i +"de la lista");
+                System.out.println("Element ToDo 2 have been found in position " + i +" of the list");
             }
-            System.out.println("El elemento ToDo 2 no se encuentra en la posicion " + i + " de la lista.");
-        }
+        }System.out.println("Element ToDo 2 is not in the list");
     }
 
     @Test
     public void testcase3 () throws InterruptedException{
-        System.out.println("Aqui va el test 3");
+        System.out.println("Starting test case 3");
 
-        //Seleccionar el ToDo3
+        //Mark the element “ToDo 3” as complete
         driver.findElement(By.xpath("/html/body/ng-view/section/section/ul/li[3]/div/input")).click();
 
         Thread.sleep(3000);
@@ -161,13 +119,12 @@ public class MyTestingClass {
         driver.findElement(By.xpath("/html/body/ng-view/section/footer/ul/li[2]/a")).click();
         Thread.sleep(3000);
 
-        //Vamos a imprimir por consola lo que hay en cada posicion
-        System.out.println("Estos son los elementos en cada posision");
+        //Print in the console the elements listed with the format: "[Position]- [Element Name]"
+        System.out.println("These are the elements in the list");
         for (int i=1; i<3; i++){
             String element = driver.findElement(By.xpath("/html/body/ng-view/section/section/ul/li["+ i + "]/div/label")).getText();
             System.out.println( i + " - " + element);
         }
-        System.out.println("Termino el test 3");
 
     }
 
